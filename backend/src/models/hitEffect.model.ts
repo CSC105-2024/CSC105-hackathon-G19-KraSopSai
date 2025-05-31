d66d2e0e06f5299f1e516cb9e802b7dd04904e39
BBelleysp <yossapan.r@gmail.com>
2025-05-31 16:22:49 +0700

    feat: add model hitEffect

--- backend/src/models/hitEffect.model.ts
+++ backend/src/models/hitEffect.model.ts
@@ -2,7 +2,24 @@ import { db } from "../index.ts";
 
 export const JobModel = {
   getAll: () => db.hitEffect.findMany(),
-  getById: (id: number) => db.hitEffect.findUnique({ where: { id } }),
-  create: (data: { title: string; description: string }) => db.hitEffect.create({ data }),
-  delete: (id: number) => db.hitEffect.delete({ where: { id } }),
+
+  getById: (id: number) =>
+    db.hitEffect.findUnique({
+      where: { id },
+    }),
+
+  create: (data: { title: string; victimId: number }) =>
+    db.hitEffect.create({
+      data: {
+        title: data.title,
+        victim: {
+          connect: { id: data.victimId },
+        },
+      },
+    }),
+
+  delete: (id: number) =>
+    db.hitEffect.delete({
+      where: { id },
+    }),
 };
