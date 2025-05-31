154175594a4bc98e61a29b9bac680fa8b9a1c7b1
BBelleysp <yossapan.r@gmail.com>
2025-05-31 14:50:45 +0700

    style: update css

--- frontend/src/pages/UserDetail.jsx
+++ frontend/src/pages/UserDetail.jsx
@@ -15,8 +15,6 @@ function UserDetail() {
     const handleSaveData = (data) => {
         setSavedData(data);
         console.log('Saved data:', data);
-        // Here you can send data to your backend, save to localStorage, etc.
-        // Note: Image is stored as base64 string in memory only
     };
   const hateList = [
   {
@@ -73,23 +71,23 @@ function UserDetail() {
     <div className="bg-[url('/images/background.jpg')] bg-cover bg-center h-screen flex flex-col">
     
     <div className='pt-6 sm:pt-10 mx-10 sm:mx-20 flex flex-col flex-1'>
-      <div className='flex justify-between items-center bg-custom-lightgradient w-full max-w-[346px] h-[83px] rounded-[20px] border-1 border-black sm:max-w-[1069px] sm:h-[100px] px-10 sm:px-20 mx-auto my-4'>
+      <div className='flex justify-between items-center bg-custom-lightgradient w-full max-w-[346px] h-[83px] rounded-[20px] border-1 border-black sm:max-w-[1069px] sm:h-[100px] px-6 sm:px-10 mx-auto my-4'>
           <h1 className='text-md sm:text-xl '>username</h1>
           <div>
-            <button className='flex sm:hidden bg-black rounded-full w-10 h-10 items-center justify-center'>
+            <button className='flex sm:hidden bg-black rounded-[10px] w-10 h-10 items-center justify-center'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" >
   <path d="M20 12H10.5M18 15L21 12L18 9M13 7V6C13 5.46957 12.7893 4.96086 12.4142 4.58579C12.0391 4.21071 11.5304 4 11 4H6C5.46957 4 4.96086 4.21071 4.58579 4.58579C4.21071 4.96086 4 5.46957 4 6V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H11C11.5304 20 12.0391 19.7893 12.4142 19.4142C12.7893 19.0391 13 18.5304 13 18V17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
             </button>
-            <button className='hidden sm:block bg-black text-white rounded-[20px] px-4 py-2'>LOGOUT</button>
+            <button className='hidden sm:block bg-black text-white rounded-[14px] px-4 py-2'>LOGOUT</button>
           </div>
       </div>
 
       <div className='flex flex-col flex-1 min-h-0'>
-        <div className='max-w-[342px] sm:max-w-[1069px] bg-lpink rounded-t-[20px] border-1 border-black flex items-center px-10 sm:px-20 mx-auto w-full'>
+        <div className='max-w-[342px] sm:max-w-[1069px] bg-lpink rounded-t-[20px] border-1 border-black flex items-center px-6 sm:px-10 mx-auto w-full'>
           <div className='flex items-center justify-between w-full py-4'>
             <h3 className='text-md sm:text-2xl '>Hate List</h3>
-            <button className='w-8 h-8 sm:w-10 sm:h-10 rounded-[20px] bg-black flex items-center justify-center'>
+            <button className='w-8 h-8 sm:w-10 sm:h-10 rounded-[10px] bg-black flex items-center justify-center'>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path d="M12 5V19M5 12H19" stroke="white" strokeWidth="2" strokeLinecap="round"/>
               </svg>
@@ -99,16 +97,16 @@ function UserDetail() {
 
         <div className='bg-white max-w-[342px] rounded-b-[20px] border-1 border-black flex flex-col sm:max-w-[1069px] flex-1 pt-2 sm:pt-4 px-4 mx-auto w-full max-h-110 overflow-y-scroll'>
           {hateList.map((hate, index) => (
-            <div key={`${hate.id}-${index}`}        className="hate-item bg-yellow flex flex-col items-center justify-between sm:max-w-[1024px] w-full min-h-[80px] sm:min-h-[100px] border-1 border-black rounded-[20px] px-6 sm:px-10 my-2 mx-auto">
+            <div key={`${hate.id}-${index}`}        className="hate-item bg-yellow flex flex-col items-center justify-between sm:max-w-[1024px] w-full min-h-[98px] sm:min-h-[100px] border-1 border-black rounded-[20px] px-6 sm:px-10 my-2 mx-auto ">
               <div className='flex items-start sm:items-center flex-col sm:flex-row sm:justify-between w-full h-full pt-2 sm:pt-0'>
-                <p className='text-md sm:text-xl '>{hate.name}</p>
-                <div className='flex items-center gap-2 w-full sm:w-50 py-2'>
+                <p className='text-md sm:text-xl pb-2'>{hate.name}</p>
+                <div className='flex items-center gap-2 w-full sm:w-50 '>
                   <button 
-                      className='bg-lblue text-black rounded-[20px] px-4 py-2 border w-full' onClick={handleOpenSetting} 
+                      className='bg-lblue text-black rounded-[14px] px-4 py-2 border w-full' onClick={handleOpenSetting} 
                     >Edit</button>
 
                         <button 
-                          className='bg-dpink text-white rounded-[20px] px-4 py-2 border w-full' 
+                          className='bg-dpink text-white rounded-[14px] px-4 py-2 border w-full' 
                           
                         >
   Forgive
@@ -118,7 +116,7 @@ function UserDetail() {
               </div>
             </div>
           ))}
-<SettingPopup
+            <SettingPopup
                 isOpen={isSettingOpen}
                 onClose={handleCloseSetting}
                 onSave={handleSaveData}
