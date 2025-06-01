d7ff72603b61c44be9d5debf6fe14e6f7f4445fe
NPC-PATRICK <patnpc1603@gmail.com>
2025-06-01 09:15:36 +0700

    fix:auth login

--- frontend/src/utils/checkAuth.jsx
+++ frontend/src/utils/checkAuth.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-    const token = localStorage.getItem("authToken");
+    const token = localStorage.getItem("token");
 
     if (!token) {
         return <Navigate to="/auth" replace />;
